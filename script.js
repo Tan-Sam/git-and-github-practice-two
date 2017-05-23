f789db7ec8fdd16fb5e5e3830cc8886dbf2c3e8d
CharlesChiaYH <CharlesChiaYH@users.noreply.github.com>
2017-05-23 16:30:23 +0800

    changes to script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ window.onload = function(){
     prompt('What is your email?')
   });
 }
+window.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?');
+    button.innerHTML = "Thanks for your email!"
+  });
+}
